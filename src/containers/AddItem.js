64e18f04e8a9ff88157ce97fd87105daf1cea720
Khizar <khizar.manzoor@hotmail.com>
2017-06-13 08:02:59 -0700

    connect properties and actions callbacks

--- src/containers/AddItem.js
+++ src/containers/AddItem.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addMovie } from '../actions'
+import RaisedButton from 'material-ui/RaisedButton'
 
 let AddItem = ( {dispatch} ) => {
+  let title, genre;
+
   return (
-    <button onClick={() => {
-      dispatch(addMovie('abc', 'cde'))
-    }}>Add</button>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      dispatch(addMovie({title: title.value, genre: genre.value}))
+    }}>
+
+      <input ref={ (node) => {
+        title = node
+      }}/>
+      <input ref={ (node) => {
+        genre = node
+      }}/>
+
+      <RaisedButton type='submit'>Add</RaisedButton>
+    </form>
   )
 }
-
 
 AddItem = connect()(AddItem)
 
